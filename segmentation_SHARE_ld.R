86af942187c69f707087555320bd36c39983ae2a
Duminy Lize <dil4@r.dil4.bfh.science>
2020-02-14 15:37:20 +0100

    Finished finding questions for ld segmentation

--- segmentation_SHARE_ld.R
+++ segmentation_SHARE_ld.R
@@ -33,7 +33,6 @@ datGS<-read_dta("//bfh.ch/data/LFE/G/Research-HE/data/SHARE/rel7-0-0/sharew7_rel
 datPH<-read_dta("//bfh.ch/data/LFE/G/Research-HE/data/SHARE/rel7-0-0/sharew7_rel7-0-0/sharew7_rel7-0-0_ALL_datasets_stata/sharew7_rel7-0-0_ph.dta")
 datCV<-read_dta("//bfh.ch/data/LFE/G/Research-HE/data/SHARE/rel7-0-0/sharew7_rel7-0-0/sharew7_rel7-0-0_ALL_datasets_stata/sharew7_rel7-0-0_cv_r.dta")
 
-
 dat_hand_grip<-read_csv("//bfh.ch/data/LFE/Users/dil4/health_states/health_states/HandGrip_Swiss.csv")
 
 
@@ -46,8 +45,8 @@ datGS<-datGS %>% filter(as_factor(datGS$country)=="Switzerland")
 datPH<-datPH %>% filter(as_factor(datPH$country)=="Switzerland")
 datCV<-datCV %>% filter(as_factor(datCV$country)=="Switzerland")
 
-#Keep only correspondants of current wave as well as deceased correspondants
-datCV<-rbind(datCV[datCV$deceased==1,],datCV[datCV$age_int!=-9,])
+#Keep only correspondants of current wave as well as people confirmed as deceased between previous wave and this wave
+datCV<-rbind(datCV[datCV$interview==1,],datCV[datCV$interview==2,])
 
 
 #Remove variables that are duplicated accross datasets
@@ -201,10 +200,6 @@ dat_SHARE_ld$d<-ifelse(dat_SHARE_ld$ph089d1==1,1,0)
 #-1   Don't know
 # 0 Not selected
 # 1     Selected
-
-
-####Joël, please double-check the chronic illnesses specified. The numbers are not the same as your pdf. 
-#If you change something, please be consistent and change it in both functions here.
 
 dat_SHARE_ld$e<-ifelse( (dat_SHARE_ld$ph006d1==1
                          |dat_SHARE_ld$ph006d2==1
@@ -413,11 +408,11 @@ table(dat_SHARE_ld$j)
 #j#############Frailty Indicators
 
 #Frailty was defined as a clinical syndrome in which three or more of the following criteria were present: 
-#unintentional weight loss (10 lbs in past year), 
-#self-reported exhaustion, 
-#weakness (grip strength), 
-#slow walking speed, 
-#and low physical activity.
+#(yes) unintentional weight loss (10 lbs in past year), 
+#(yes) self-reported exhaustion, 
+#(yes) weakness (grip strength), 
+#(no)  slow walking speed, 
+#(yes) and low physical activity.
 
 #Fried, L.P., Tangen, C.M., Walston, J., Newman, A.B., Hirsch, C., Gottdiener, J., Seeman, T., Tracy, R., 
 #Kop, W.J., Burke, G. and McBurnie, M.A., 2001. 
@@ -425,7 +420,6 @@ table(dat_SHARE_ld$j)
 #The Journals of Gerontology Series A: Biological Sciences and Medical Sciences, 56(3), pp.M146-M157.
 
 
-###Cognitive function <- Differences in cognitive performance and cognitive decline across European regions: a population-based prospective cohort study
 
 ########################################################################
 #j1############Hand Grip Strength
@@ -533,7 +527,7 @@ table(dat_SHARE_ld$health_state)
 dat_SHARE_ld$j2<-ifelse(dat_SHARE_ld$ph003_==5,1,0)
 
 
-#table(dat_SHARE_ld$j2)
+table(dat_SHARE_ld$j2)
 #j2=1  self-assessed health rated as poor
 #j2=0  self-assessed health rated as not poor
 #      & Don't know + Refusal
@@ -541,28 +535,136 @@ dat_SHARE_ld$j2<-ifelse(dat_SHARE_ld$ph003_==5,1,0)
 
 
 ########################################################################
-#j3############PH003_HealthGen2
-#Würden Sie sagen, Ihr Gesundheitszustand ist...
+#j3############Physical Activity (BR015_PartInVigSprtsAct & BR016_ModSprtsAct)
 
-#Would you say that your health is...
+#Measure physical activity the same way as: Gomes, M., Figueiredo, D., Teixeira, L., Poveda, V., Paúl, C., Santos-Silva, A. and Costa, E., 2017. 
+#Physical inactivity among older adults across Europe based on the SHARE database. Age and ageing, 46(1), pp.71-77.
+#Physical inactivity was assessed via the following questions: ‘How often do you engage in activities that require a moderate level of energy 
+#such as gardening, cleaning the car, or doing a walk?’ and ‘We would like to know about the type and amount of physical activity you do in 
+#your daily life. How often do you engage in vigorous physical activity, such as sports, heavy housework, or a job that involves physical labour?’ 
+#Both questions had as possible answers: ‘More than once a week’, ‘Once a week’, ‘One to three times a month’ and ‘Hardly ever, or never’. 
+#Physical inactivity was defined as never or almost never engaging in moderate or vigorous physical activity through the response of ‘One to 
+#three times a month’ and ‘Hardly ever, or never’ to both questions.
 
 
-#list(dat_SHARE_ld$ph003_)
+#j3a############BR015_PartInVigSprtsAct
+
+#Wir würden gerne wissen, wie und wie häufig Sie sich im Alltag körperlich betätigen. Wie häufig üben Sie im Alltag
+#eine @Banstrengende körperliche Tätigkeit@B aus, wie zum Beispiel beim Sport, bei schweren Arbeiten in Haus und
+#Garten oder bei der Ausübung von einem Beruf, bei dem man körperlich schwer arbeitet.
+
+#We would like to know how and how often you are physically active in everyday life. How often do you carry out 
+#@exhausting physical activity@B in everyday life, such as sports, heavy work in the house and garden, or when you 
+#have a job where you work hard.
+
+
+#list(dat_SHARE_ld$br015_)
+#value                    label
+#-2                    Refusal
+#-1                 Don't know
+# 1      More than once a week
+# 2                Once a week
+# 3 One to three times a month
+# 4      Hardly ever, or never
+
+#table(dat_SHARE_ld$br015_)
+#TABLE
+#-2   1   2   3   4 
+# 1 299 133  74 246 
+
+dat_SHARE_ld$j3a<-ifelse(dat_SHARE_ld$br015_==3|dat_SHARE_ld$br015_==4,1,0)
+
+
+table(dat_SHARE_ld$j3a)
+#j3a=1  Does not often do vigorous physical activity
+#j3a=0  Ofted does vigorous physical activity
+#       & Don't know + Refusal
+
+
+#j3b############BR016_ModSprtsAct
+
+#Wie häufig machen Sie Dinge, wo @bleicht@b anstrengend sind, wie zum Beispiel leichte Gartenarbeit, das Auto waschen oder spazieren gehen?
+
+#How often do you do things where @bleach@b is strenuous, such as light gardening, washing the car or going for a walk?
+
+
+#list(dat_SHARE_ld$br016_)
+#value                    label
+#-2                    Refusal
+#-1                 Don't know
+# 1      More than once a week
+# 2                Once a week
+# 3 One to three times a month
+# 4      Hardly ever, or never
+
+#table(dat_SHARE_ld$br016_)
+#TABLE
+#-2   1   2   3   4 
+# 1 570  90  31  61 
+
+dat_SHARE_ld$j3b<-ifelse(dat_SHARE_ld$br016_==3|dat_SHARE_ld$br016_==4,1,0)
+
+
+#table(dat_SHARE_ld$j3b)
+#j3a=1  Does not often do light physical activity
+#j3a=0  Ofted does light physical activity
+#       & Don't know + Refusal
+
+dat_SHARE_ld$j3<-ifelse(dat_SHARE_ld$j3a==1&dat_SHARE_ld$j3b==1,1,0)
+
+table(dat_SHARE_ld$j3)
+#j3=1  Does not often do physical activity
+#ja=0  Ofted does physical activity
+#       & Don't know + Refusal
+
+
+########################################################################
+########################################################################
+###Cognitive function 
+#Used the same cognative performance metric as the following paper (which also uses SHARE data)
+#Formanek, T., Kagstrom, A., Winkler, P. and Cermakova, P., 2019. Differences in cognitive performance and cognitive decline across European regions: 
+#a population-based prospective cohort study. European Psychiatry, 58, pp.80-86.
+#Differences in cognitive performance and cognitive decline across European regions: a population-based prospective cohort study
+#
+#NOTE: Cognition was not tested in wave 3!!! Cognitive is tested in waves 1, 2, 4, 5 and 6.
+
+#VERBAL FLUENCY <- animal fluency test [Henley NM. A psychological study of the semantics of animal terms. J Verbal Learning Verbal Behav 1969;8:176–84]
+#The participants were asked to name as many different animals as they could think of within one minute. 
+#The verbal fluency score was the sum of acceptable animals.
+
+#IMMEDIATE RECALL <- Immediate and delayed recall were extracted from an adapted 10-word delay recall test [Harris SJ, Dowson JH. Recall of a 10-word list in the assessment of dementia in the elderly. Br J Psychiatry 1982;141:524–7]. 
+#The format of the test used in SHARE is based on the Telephone Interview of Cognitive Status-Modified (TICS-M) [Brandt J, Spencer M, Folstein M. The telephone interview for cognitive status. Neuropsychiatry Neuropsychol Behav Neurol 1988;1:111–7.].
+#Immediate recall score (range 0–10) was the number of recalled words after the interviewer read a list of 10 words from their computer screen.
+
+#DELAYED RECALL <- At the end of the cognitive testing session, the participants were asked again to recall any of the words from the list, which captured delayed recall score (range 0–10).
+
+
+########################################################################
+#m1############CF010_Animals
+#Das Ergebnis ergibt sich aus der Summe aller akzeptablen Bezeichnungen für Tiere. Als korrekt gelten alle Vertreter
+#des Tierreichs; real existierende und mythologische, nicht aber Wiederholungen und Eigennamen. Im Einzelnen
+#heisst das, dass auch die Folgenden als korrekt zählen: Bezeichnungen von Tierarten und Rassen innerhalb einer
+#Art; Bezeichnungen für männliche und weibliche Tiere und deren Nachkommen innerhalb einer Art. Anzahl der Tiere
+#kodieren (0..100)
+
+#The result is the sum of all acceptable animal names. All representatives of the animal kingdom are considered correct; 
+#real existing and mythological, but not repetitions and proper names. In detail this means that the following also count 
+#as correct: Names of animal species and breeds within a species; names of male and female animals and their offspring 
+#within a species. Code number of animals (0..100)
+
+
+
+list(dat_SHARE_ld$cf010_)
 #value      label
 #-2    Refusal
 #-1 Don't know
-# 1  Excellent
-# 2  Very good
-# 3       Good
-# 4       Fair
-# 5       Poor
 
-#table(dat_SHARE_ld$ph003_)
+table(dat_SHARE_ld$cf010_)
 #TABLE
-#-2   -1    1    2    3    4    5 
-# 1    4  230  609 1024  416  111 
+#-1  0  1  2  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 42 45 46 47 50 
+##1  1  1  1  2  1  1  4  6  7 12 12 16 23 30 44 30 37 44 34 45 46 39 44 41 30 32 29 29 18 20  9 11  8 11  7  4  5  4  1  2  1  1  1  1  1
 
-dat_SHARE_ld$j2<-ifelse(dat_SHARE_ld$ph003_==5,1,0)
+#dat_SHARE_ld$j2<-ifelse(dat_SHARE_ld$ph003_==5,1,0)
 
 
 #table(dat_SHARE_ld$j2)
@@ -570,6 +672,40 @@ dat_SHARE_ld$j2<-ifelse(dat_SHARE_ld$ph003_==5,1,0)
 #j2=0  self-assessed health rated as not poor
 #      & Don't know + Refusal
 
+
+#########################Dementia
+#n############PH006_DocCond
+#Bitte schauen Sie sich Karte 32 an.
+#[@ Hat Ihnen ein Arzt jemals gesagt, dass Sie/@ Ist es so, dass Sie zurzeit] unter einer von den Krankheiten leiden,
+#wo dort aufgeführt sind? [@ Damit ist gemeint, dass Ihnen ein Arzt die Diagnose gestellt hat und dass Sie zurzeit
+#entsprechend behandelt werden oder von der Krankheit beeinträchtigt sind.] Bitte nennen Sie mir die
+#entsprechende(n) Ziffer(n).
+
+#Please take a look at map 32.
+#[@ Has any doctor ever told you that you/@ Are you currently] suffering from any of the diseases
+#where it is listed? @ This means that a doctor has diagnosed you and that you are currently
+#are treated accordingly or are affected by the disease]. Please name the
+#corresponding figure(s).
+
+#list(dat_SHARE_ld$ph006d1:ph006d21)
+#  value  label
+#-2      Refusal
+#-1   Don't know
+# 0 Not selected
+# 1     Selected
+
+table(dat_SHARE_ld$ph006d16)
+#TABLE
+#  -2   -1    0    1 
+#   1    2 2359   31 
+
+dat_SHARE_ld$n<-ifelse(dat_SHARE_ld$ph006d16==1&dat_SHARE_ld$interview==1,1,0)
+
+
+#table(dat_SHARE_ld$n)
+#n=1  Doctor has diagnosed patient with Alzheimer's disease, dementia, organic psychosyndrome, senility or other severe memory disorders & person is currently alive
+#n=0  No diagnosis
+#     & Don't know + Refusal
 
 
 
